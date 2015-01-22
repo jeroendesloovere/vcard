cab413eb17a49bf7a3c0f683740d86ccd1596f14
Jeroen Desloovere <jeroendesloovere@gmail.com>
2015-01-22 11:08:51 +0100

    ExceptionTest added

---
+++ tests/ExceptionTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace JeroenDesloovere\VCard;
+
+// required to load
+require_once __DIR__ . '/../vendor/autoload.php';
+
+/*
+ * This file is part of the VCard PHP Class from Jeroen Desloovere.
+ *
+ * For the full copyright and license information, please view the license
+ * file that was distributed with this source code.
+ */
+
+use JeroenDesloovere\VCard\Exception;
+
+/**
+ * VCard Exception Test.
+ *
+ * @author Jeroen Desloovere <info@jeroendesloovere.be>
+ */
+class ExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException Exception
+     */
+    public function testException()
+    {
+        throw new Exception('Testing the VCard error.');
+    }
+}
