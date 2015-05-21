9b25abd8b2b6adb63ebfada4ae2ebd5f061cfee7
Jeroen Desloovere <jeroendesloovere@gmail.com>
2015-05-21 08:21:14 +0200

    Test: Removed unused use class Exception

--- tests/ExceptionTest.php
+++ tests/ExceptionTest.php
@@ -12,7 +12,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
  * file that was distributed with this source code.
  */
 
-use JeroenDesloovere\VCard\Exception;
 
 /**
  * VCard Exception Test.
