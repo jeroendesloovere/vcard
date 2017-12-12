6ce48e1aa81b346b9b51d2ab65f8a723d6b0a4e4
Jeroen Desloovere <info@jeroendesloovere.be>
2017-12-12 16:53:41 +0100

    Tests updated

--- tests/VCard/Formatter/FormatterTest.php
+++ tests/VCard/Formatter/FormatterTest.php
@@ -2,6 +2,8 @@
 
 namespace JeroenDesloovere\VCard\Formatter;
 
+use JeroenDesloovere\VCard\Parser\Parser;
+use JeroenDesloovere\VCard\Parser\VcfParser;
 use JeroenDesloovere\VCard\Property\Address;
 use JeroenDesloovere\VCard\Property\Name;
 use JeroenDesloovere\VCard\Property\Parameter\Type;
@@ -11,20 +13,37 @@ require_once __DIR__ . '/../../../vendor/autoload.php';
 
 class FormatterTest extends \PHPUnit_Framework_TestCase
 {
-    public function testVcfFormatter()
+    /** @var VCard */
+    private $firstVCard;
+
+    /** @var VCard */
+    private $secondVCard;
+
+    public function setUp()
     {
         // Building one or multiple vCards
-        $firstVCard = (new VCard())
+        $this->firstVCard = (new VCard())
             ->add(new Name('Jeroen', 'Desloovere'))
             ->add(new Address(null, null, 'Markt 1', 'Brugge', 'West-Vlaanderen', '8000', 'België', Type::work()));
-        $secondVCard = (new VCard())
+        $this->secondVCard = (new VCard())
             ->add(new Name('John', 'Doe'))
             ->add(new Address(null, 'Penthouse', 'Korenmarkt 1', 'Gent', 'Oost-Vlaanderen', '9000', 'België', Type::work()));
+    }
 
-        // Saving to .vcf file
-        $formatter = new Formatter(new VcfFormatter(), 'export-multiple-vcf-cards');
-        $formatter->addVCard($firstVCard);
-        $formatter->addVCard($secondVCard);
+    public function testSavingMultipleVCardsToVcfFile()
+    {
+        $formatter = new Formatter(new VcfFormatter(), 'vcards');
+        $formatter->addVCard($this->firstVCard);
+        $formatter->addVCard($this->secondVCard);
         $formatter->save(__DIR__);
+    }
+
+    public function testParsingMultipleVCardsFromVcfFile()
+    {
+        $parser = new Parser(new VcfParser(), Parser::getFileContents(__DIR__ . '/vcards.vcf'));
+
+        // @todo
+        //$this->assertEquals($this->firstVCard, $parser->getVCards()[0]);
+        //$this->assertEquals($this->secondVCard, $parser->getVCards()[1]);
     }
 }
