4a47201fb635abb361d5463463800e0a4c05b5da
Melroy van den Berg <webmaster1989@gmail.com>
2019-03-01 19:16:01 +0100

    Add type to telephone parser

--- src/Parser/Property/TelephoneParser.php
+++ src/Parser/Property/TelephoneParser.php
@@ -6,11 +6,18 @@ namespace JeroenDesloovere\VCard\Parser\Property;
 
 use JeroenDesloovere\VCard\Property\NodeInterface;
 use JeroenDesloovere\VCard\Property\Telephone;
+use JeroenDesloovere\VCard\Property\Parameter\Type;
 
 final class TelephoneParser extends PropertyParser implements NodeParserInterface
 {
     public function parseVcfString(string $value, array $parameters = []): NodeInterface
     {
-        return new Telephone(str_replace('tel:', '', $value));
+        $telephone = new Telephone(str_replace('tel:', '', $value));
+        
+        if (array_key_exists(Type::getNode(), $parameters)) {
+            $telephone->setType($parameters[Type::getNode()]);
+        }
+        
+        return $telephone;
     }
 }
